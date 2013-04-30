f7cad49be15b3ab27ff8a0b4c137209b8dc3d0d3
saul coria <coriasaul@gmail.com>
2013-04-30 07:36:21 -0300

    extructura con metodos del commitData y GuideJsonParse

--- src/rio/cuarto/service/CommitData.java
+++ src/rio/cuarto/service/CommitData.java
@@ -1,12 +1,60 @@
-/**
- * 
- */
 package rio.cuarto.service;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import rio.cuarto.database.IWriteOnGuide;
+
 /**
- * @author edu
- *
+ * CommitData es la clase encargada de guardar los datos en la base de datos
+ * usando la interface IWeiteOnGuide ,cada metodo toma un JSONArray que trae los
+ * datos especificos para cada metodo
+ * 
+ * 
  */
 public class CommitData {
+
+	public CommitData(IWriteOnGuide iwriteOnGuide) {
+	}
+
+	// --------------- category ---------------------------
+
+	// guarda en la base de datos categorias nuevas , en objeto Json viene con
+	// todas las categorias a guardar
+	public void categoryAdd(JSONArray guideJsonArray) {
+
+	}
+
+	public void categoryDelete(JSONArray guideJsonArray) {
+	}
+
+	public void categoryUpdate(JSONArray guideJsonArray) {
+	}
+
+	// ---------------------subcategory--------------------------------
+	public void subCategoryAdd(JSONArray guideJsonArray) {
+	}
+
+	public void subCategoryDelete(JSONArray guideJsonArray) {
+	}
+
+	// ----------------advertisement--------------------------------
+	public void advertisementAdd(JSONArray guideJsonArray) {
+	}
+
+	public void advertisementUpdate(JSONArray guideJsonArray) {
+	}
+
+	public void advertisementDelete(JSONArray guideJsonArray) {
+	}
+
+	// ---------------------HAVE-----------------------------------------
+
+	public void AdvHaveCategoryAdd(JSONArray guideJsonArray) {
+	}
+
+	public void AdvHaveCategoryDelete(JSONArray guideJsonArray) {
+	}
 
 }
---
+++ src/rio/cuarto/service/GuideJsonParser.java
@@ -0,0 +1,73 @@
+package rio.cuarto.service;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * la clase GuideJsonParser proporciona arreglo de datos (JSONArray) especificos
+ * ejemplo : getJSONArrayCategoryAdd() retorna todas las categorias a guardar
+ * 
+ */
+public class GuideJsonParser {
+
+	public GuideJsonParser(byte[] byteArray) {
+	}
+
+	// get JSONArray of the Category
+
+	public JSONArray getJSONArrayCategoryAdd() {
+		return null;
+	}
+
+	public JSONArray getJSONArrayCategoryDelete() {
+		return null;
+	}
+
+	public JSONArray getJSONArrayCategoryUpdate() {
+		return null;
+	}
+
+	// get JSONArray of the SubCategory
+
+	public JSONArray getJSONArraySubCategoryAdd() {
+		return null;
+	}
+
+	public JSONArray getJSONArraySubCategoryDelete() {
+		return null;
+	}
+
+	// get JSONArry of the Advertisement
+
+	public JSONArray getJSONArrayAdvertisementAdd() {
+		return null;
+	}
+
+	public JSONArray getJSONArrayAdvertisementDelete() {
+		return null;
+	}
+
+	public JSONArray getJSONArrayAdvertisementUpdate() {
+		return null;
+	}
+
+	// get JSONArray of the Have
+
+	public JSONArray getJSONArrayHaveAdd() {
+		return null;
+	}
+
+	public JSONArray getJSONArrayHaveDelete() {
+		return null;
+	}
+
+	// metodo usado por los getJsonArray
+	// toma un string que es la category o advertisement o have o subcategory
+	// que indica que
+	// JSONArray va retornar
+	private JSONArray getJsonArray(String string) {
+		return null;
+	}
+
+}
